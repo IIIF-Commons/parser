17055626e8e9360b009683710720ed23ca86c35b
Stephen Fraser <stephen.fraser@digirati.com>
2023-04-03 08:01:19 +0100

    Addition to specific resource check

--- src/shared/is-specific-resource.ts
+++ src/shared/is-specific-resource.ts
@@ -1,5 +1,10 @@
 import { SpecificResource } from '@iiif/presentation-3';
 
 export function isSpecificResource(resource: unknown): resource is SpecificResource {
-  return (resource as any).type === 'SpecificResource';
+
+  if (typeof resource === 'string') {
+    return false;
+  }
+
+  return !!resource && (resource as any).type === 'SpecificResource';
 }
