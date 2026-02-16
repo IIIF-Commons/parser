6a0bcba940ad953009850f0dbc9689375101accd
Stephen Fraser <stephen.fraser@digirati.com>
2026-02-16 18:52:32 +0000

    Normalized type tests

--- src/presentation-4-normalized/types/legacy/iiif/technical-v4.d.ts
+++ src/presentation-4-normalized/types/legacy/iiif/technical-v4.d.ts
@@ -1,30 +1,52 @@
-export type NormalizedReference =
-  | string
-  | {
-      id?: string;
-      type?: string;
-      [key: string]: unknown;
-    };
+import type { MetadataItem } from "../../../../presentation-4/types/legacy/src/resources/contentResource";
 
-export type NormalizedFramingPart = {
-  id?: string;
-  type?: string;
-  "@explicit"?: boolean;
-  "iiif-parser:partOf"?: string;
-  [key: string]: unknown;
-};
+export type NormalizedPrimitive = string | number | boolean | null;
+export type NormalizedJsonValue = NormalizedPrimitive | NormalizedJsonValue[] | { [key: string]: NormalizedJsonValue };
 
-export interface NormalizedEntityBase {
+export interface NormalizedReferenceObject {
   id?: string;
   type?: string;
   "@id"?: string;
   "@type"?: string;
-  "iiif-parser:hasPart"?: NormalizedFramingPart[];
-  [key: string]: unknown;
+  [key: string]: NormalizedJsonValue | NormalizedReference | readonly NormalizedReference[] | undefined;
+}
+
+export interface NormalizedSpecificResourceReference extends NormalizedReferenceObject {
+  type?: "SpecificResource";
+  source?: NormalizedReference | readonly NormalizedReference[];
+  selector?: readonly NormalizedReference[];
+  transform?: readonly NormalizedReference[];
+  action?: readonly NormalizedReference[];
+  purpose?: readonly string[];
+  scope?: NormalizedReference | string | readonly (NormalizedReference | string)[];
+  styleClass?: string;
+}
+
+export type NormalizedReference = string | NormalizedReferenceObject | NormalizedSpecificResourceReference;
+
+export interface NormalizedFramingPart extends NormalizedReferenceObject {
+  "@explicit"?: boolean;
+  "iiif-parser:partOf"?: string;
+  [key: string]:
+    | NormalizedJsonValue
+    | NormalizedReference
+    | readonly NormalizedReference[]
+    | readonly NormalizedFramingPart[]
+    | undefined;
+}
+
+export interface NormalizedEntityBase extends NormalizedReferenceObject {
+  "iiif-parser:hasPart"?: readonly NormalizedFramingPart[];
+  [key: string]:
+    | NormalizedJsonValue
+    | NormalizedReference
+    | readonly NormalizedReference[]
+    | readonly NormalizedFramingPart[]
+    | undefined;
 }
 
 export interface NormalizedLinkedEntity extends NormalizedEntityBase {
-  metadata: readonly unknown[];
+  metadata: readonly MetadataItem[];
   provider: readonly NormalizedReference[];
   thumbnail: readonly NormalizedReference[];
   behavior: readonly string[];
