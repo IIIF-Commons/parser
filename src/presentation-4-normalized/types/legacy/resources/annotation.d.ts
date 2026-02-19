521386db24ca859e848a4be274b2aa546f917600
Stephen Fraser <stephen.fraser@digirati.com>
2026-02-19 11:55:36 +0000

    Update annotation.d.ts

--- src/presentation-4-normalized/types/legacy/resources/annotation.d.ts
+++ src/presentation-4-normalized/types/legacy/resources/annotation.d.ts
@@ -1,8 +1,15 @@
-import type { NormalizedLinkedEntity, NormalizedReference } from "../iiif/technical-v4";
+import type { NormalizedJsonValue, NormalizedLinkedEntity, NormalizedReference } from "../iiif/technical-v4";
+
+export type AnnotationTargetNormalized =
+  | NormalizedReference
+  | ({
+      id: string;
+      type: string;
+    } & Record<string, NormalizedJsonValue>);
 
 export type AnnotationNormalized = NormalizedLinkedEntity & {
   type: "Annotation";
   motivation: readonly string[];
   body: NormalizedReference | null;
-  target: NormalizedReference;
+  target: AnnotationTargetNormalized;
 };
